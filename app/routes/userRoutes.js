87a80d5a21488030db8c4e37c704f07356e99b6e
John Ralph Alcantara <alcantarajohnralph@gmail.com>
2026-03-23 14:25:18 +0800

    feat: add input validator for user using joi object schema validatation

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from '../controller/userController.js';
+import validateUser from '../middlewares/inputValidator.js';
 
 const router = express.Router();
 
-router.post("/users", createUser);
+router.post("/users", validateUser, createUser);
 router.get("/users", getAllUsers);
 router.get("/user/:id", getUserById);
-router.put("/user/:id", updateUser);
+router.put("/user/:id", validateUser, updateUser);
 router.delete("/user/:id", deleteUser);
 
 export default router;
